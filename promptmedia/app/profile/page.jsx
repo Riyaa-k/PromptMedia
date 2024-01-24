be467dc15bfbae98ca483c27658850bd54aeab2d
ABHINAV <abhinavpatel0208@gmail.com>
2024-01-24 14:47:59 +0530

    Delete Is not Working

--- promptmedia/app/profile/page.jsx
+++ promptmedia/app/profile/page.jsx
@@ -47,10 +47,40 @@ const MyProfile = () => {
     }
   };
 
+
+  // const handleDelete = async (post) => {
+  //   const hasConfirmed = confirm(
+  //     "Are you sure you want to delete this prompt?"
+  //   );
+  
+  //   if (hasConfirmed) {
+  //     try {
+  //       console.log("Deleting post:", post._id);
+  
+  //       const response = await fetch(`/api/prompt/${post._id.toString()}`, {
+  //         method: "DELETE",
+  //       });
+  
+  //       console.log("Delete response:", response);
+  
+  //       if (response.ok) {
+  //         const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+  //         setMyPosts(filteredPosts);
+  //         console.log("Post deleted successfully");
+  //       } else {
+  //         console.log("Failed to delete post");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error deleting post:", error);
+  //     }
+  //   }
+  // };
+  
+
   return (
     <Profile
-      name='My'
-      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
       data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
